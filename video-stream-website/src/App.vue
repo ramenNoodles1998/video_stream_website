6af6b4c0b0b55c76954f062563e7723e4f79fd47
Roman Meredith <10769789@uvu.edu>
2021-12-10 01:56:39 -0700

    created api where it takes whole video and saves in video directory, and save location in sql. Also created video stream api from videoDirectory

--- video-stream-website/src/App.vue
+++ video-stream-website/src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
     <div class="container">
-      <form enctype="multipart/form-data" novalidate>
-        <h1>Upload Video</h1>
-        <div class="dropbox">
-          <input type="file" :name="uploadFieldName" :disabled="isSaving" @change="fileUpload($event.target.name, $event.target.files[0])"
-            accept="mp4/*">
-        </div>
+      <form enctype="multipart/form-data" novalidate onsubmit="return false">
+        <input type="file" ref="inputFile" :disabled="isSaving" @change="fileUpload($event.target.name, $event.target.files[0])" accept="mp4/*">
+
+        <button @click="fileSubmit">Submit Video</button>
       </form>
     </div>
+
     <video id="videoPlayer" width="650" controls muted="muted" autoplay>
-      <source src="http://localhost:3000/video" type="video/mp4" />
+      <source :src="api" type="video/mp4" />
     </video>
   </div>
 </template>
 
 <script>
 //const chunk_size = 10 ** 7 //1MB
-const api = 'http://localhost:3000'
 
 export default {
   
@@ -26,27 +24,30 @@ export default {
   data: () => ({
     file: null,
     isSaving: false,
-    uploadFieldName: ''
+    videoTitle: '',
+
+    api: 'http://localhost:3000/'
   }),
-  //get file
-  //split into chunks
-  // send each chunk to backend
-  //
 
   methods: {
-    async fileUpload(fieldName, file) {
+    fileUpload(fieldName, file) {
       this.isSaving = true
       this.file = file
-      this.uploadFieldName = fieldName
 
-      //for(let i = 0; i < file.size; i += chunk_size) {
-      this.axios.post(api, file)
-      //}
-      
-      //this.blob.push(await this.axios.post(api, file.slice(file.size - (file.size % chunk_size), file.size - (file.size % chunk_size))))
+      this.isSaving = false
+    },
 
-      console.log(this.blob)
+    async fileSubmit() {
+      this.isSaving = true
+
+      await this.axios.post(this.api, this.file)
+
+      this.$refs.inputFile.value = null
+
+      this.isSaving = false
+      this.file = null
     }
+   
   }
 }
 </script>
